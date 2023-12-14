867ae81197ad8f87399822b5d92cf6ac1775a578
Winfred <winfrednginakilonzo@gmail.com>
2023-12-14 18:06:42 +0300

    Added delimiter

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,11 @@
 -- Updates valid_email after email change
 DELIMITER //
-CREATE TRIGGER aftter_email_change
+CREATE TRIGGER before_email_change
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email <> NEW.email THEN
-        SET NEW.valid_email = 0
-        
+        SET NEW.valid_email = 0;    
     END IF;
 END;
 //
