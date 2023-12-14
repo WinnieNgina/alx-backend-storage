d0bc17ce86b76a26e67ff0d855faacfb16128f82
Winfred <winfrednginakilonzo@gmail.com>
2023-12-14 14:01:15 +0300

    Calculate band life span

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- Calculate life time of bands that have Glam rock as their main style 
+SELECT 
+    band_name, 
+    (IFNULL(split, 2022) - formed) AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
