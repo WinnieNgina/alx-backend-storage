0526c035d43a145f59c292c813d1871f719cff4a
Winfred <winfrednginakilonzo@gmail.com>
2023-12-14 17:36:52 +0300

    Created a trigger

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Create a trigger to decrease quantity after adding a new order
+DELIMITER //
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON order
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER;
