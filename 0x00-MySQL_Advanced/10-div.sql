7473a5a7cbb37e1c711a419804a89078fe8b86e5
Winfred <winfrednginakilonzo@gmail.com>
2023-12-16 14:23:33 +0300

    Division function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- FUNCTION THAT divides a by b
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN 
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END//
+DELIMITER ; 
