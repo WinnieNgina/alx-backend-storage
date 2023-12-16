33a386ce250c352c674d84758d57434fe1da6248
Winfred <winfrednginakilonzo@gmail.com>
2023-12-16 14:52:43 +0300

    Fixed bug

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
