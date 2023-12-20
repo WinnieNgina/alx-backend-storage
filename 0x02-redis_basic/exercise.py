82f9a94f93e792aa223d3006b196e057be5f00cd
Winfred <winfrednginakilonzo@gmail.com>
2023-12-20 19:46:42 +0000

    Fix bugs

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -69,11 +69,13 @@ class Cache:
         """Retrieves data from db as int"""
         return self.get(key, fn=int)
 
-    def replay(self, func):
-        """Retrieve the call history from Redis"""
-        call_history = self.redis_client.lrange(self.call_history_key, 0, -1)
-        # Unpack the call history using zip and iterate over inputs and outputs
-        inputs, outputs = zip(*[pickle.loads(call_data) for call_data in call_history])
-        print(f"{func.__name__} was called {len(call_history)} times:")
-        for args, result in zip(inputs, outputs):
-            print(f"{func.__name__}(*{args}) -> {result}")
+    def replay(method: Callable):
+        """Displays history of a method"""
+        key = method.__qualname__
+        inputs = method.__self__.redis.lrange(f"{key}:inputs", 0, -1)
+        outputs = method.__self__.redis.lrange(f"{key}:outputs", 0, -1)
+        print(f"{key} was called {len(inputs)} times:")
+        for input, output in zip(inputs, outputs):
+        input_str = input.decode('utf-8')
+        output_str = output.decode('utf-8')
+        print(f"{key}{input_str} -> {output_str}")
